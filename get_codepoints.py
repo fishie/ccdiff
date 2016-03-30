15220d67592e709f5597c71bff725d92a504f4ee
Rishie Sharma <rishie@rishie.se>
2016-03-30 17:08:56 +0200

    Fixes #2

---
+++ get_codepoints.py
@@ -0,0 +1,22 @@
+from itertools import chain
+import sys
+import os
+import re
+
+from fontTools.ttLib import TTFont
+from fontTools.unicode import Unicode
+
+clean = lambda varStr: re.sub('\W|^(?=\d)','_', varStr)
+name = clean(os.path.splitext(os.path.basename(sys.argv[1]))[0])
+ttf = TTFont(sys.argv[1], 0, verbose=0, allowVID=0,
+                ignoreDecompileErrors=True, fontNumber=-1)
+
+chars = set(chain.from_iterable([y[0] for y in x.cmap.items()] for x in ttf["cmap"].tables))
+
+js = 'var ' + name + '_glyphdict = {'
+js += ','.join([str(x)+':1' for x in chars])
+js += '};'
+
+open(name + '.js', 'w').write(js)
+
+ttf.close()
