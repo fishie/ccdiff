3a8ee0b28a822d6750dc53c96f68d06e7155bbe8
Rishie Sharma <rishie@rishie.se>
2016-04-22 17:03:40 +0200

    Initial fix for #5

--- www/js/index.js
+++ www/js/index.js
@@ -90,9 +90,43 @@ angular.module('han').controller('MainController', function() {
         return variantChars;
     }
     
-    this.getUnicodeRepresentation = function () {
-        return 'U+' + this.chars.charCodeAt(0).toString(16).toUpperCase() +
-            ' (' + this.chars.charCodeAt(0) + ')';
+
+    
+});
+
+angular.module('han').directive('codeConverter', function() {
+    
+    var base = 0;
+    
+    function getHexadecimal(chars) {
+        return chars.charCodeAt(0).toString(16).toUpperCase();
+    }
+    
+    function getDecimal(chars) {
+        return chars.charCodeAt(0);
+    }
+    
+    function toCharFromDecimal(code) {
+        return String.fromCharCode(code);
+    }
+    
+        function toCharFromHexadecimal(code) {
+        return String.fromCharCode(parseInt(code, 16));
+    }
+    
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function(scope, element, attributes, ngModel) {
+
+            if (attributes.codeConverter == 'decimal') {
+                ngModel.$parsers.push(toCharFromDecimal);
+                ngModel.$formatters.push(getDecimal);
+            } else if (attributes.codeConverter == 'hexadecimal') {
+                ngModel.$parsers.push(toCharFromHexadecimal);
+                ngModel.$formatters.push(getHexadecimal);
+            }
+        }
     };
 });
 
