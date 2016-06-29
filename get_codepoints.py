43b9f73beac85f1fa4771a0939a2a6096fc508f0
Rishie Sharma <rishie@rishie.se>
2016-06-29 16:36:37 +0900

    get_codepoints changes
    * get_codepoints always included ASCII codepoints even though they were not present in fontforge. It is more verbose now and seems to be working properly. The reason for this behaviour could be some intricate properties about fonts I have no idea about.

--- get_codepoints.py
+++ get_codepoints.py
@@ -8,12 +8,20 @@ from fontTools.unicode import Unicode
 
 clean = lambda varStr: re.sub('\W|^(?=\d)','_', varStr)
 name = clean(os.path.splitext(os.path.basename(sys.argv[1]))[0])
-ttf = TTFont(sys.argv[1], 0, verbose=0, allowVID=0, ignoreDecompileErrors=True, fontNumber=-1)
-chars = set(chain.from_iterable([y[0] for y in x.cmap.items()] for x in ttf["cmap"].tables))
+ttf = TTFont(sys.argv[1], 0, verbose=1, allowVID=0, ignoreDecompileErrors=False, fontNumber=-1)
+for table in ttf['cmap'].tables:
+    print('Platform ID={0}'.format(table.platformID))
+    print('Is Unicode={0}'.format(table.isUnicode()))
+    print('Language={0}'.format(table.platformID))
+    print('Length={0}'.format(table.length))
+    print('cmap length={0}'.format(len(table.cmap.items())))
+    print()
+#chars = set(chain.from_iterable([y[0] for y in x.cmap.items()] for x in ttf["cmap"].tables))
+chars = sorted(set([x[0] for x in ttf['cmap'].tables[0].cmap.items()]))
 
 jsdict = '{' + ','.join([str(x)+':1' for x in chars]) + '}'
 js = 'var {0}_glyphdict = {1};'.format(name, jsdict)
 
 open(name + '.js', 'w').write(js)
-
+print(js)
 ttf.close()
